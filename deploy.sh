b5c7616f03301f6963f5ac87bbd32b82fd60370a
Augustus Ostow <ostowster@gmail.com>
2019-03-26 22:44:15 -0400

    Add payload path printing for deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ hash=$(shasum $deployments_dir/temp.zip | cut -d " " -f 1)
 name="sudoku-$date-$hash.zip"
 source_code_path=$deployments_dir/$name
 
+echo "Saving source code payload to $source_code_path..."
+
 mv $deployments_dir/temp.zip $source_code_path
 
 cd ./terraform
