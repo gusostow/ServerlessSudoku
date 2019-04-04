31bc21c89ce52f89f9be64c356110258529b8a4b
Augustus Ostow <ostowster@gmail.com>
2019-04-04 11:54:43 -0700

    Tagline and gh logo

--- static/js/script.js
+++ static/js/script.js
@@ -17,7 +17,7 @@ function get_input_board() {
 function fill_board() {
     let input_board = get_input_board()
     const payload = JSON.stringify({board: input_board})
-    const endpoint_url = "https://bkv4y1mug2.execute-api.us-east-2.amazonaws.com/development/sudoku"
+    const endpoint_url = "https://p3mto0h4h9.execute-api.us-east-2.amazonaws.com/development/sudoku"
     $.post(endpoint_url, payload, function(data) {
         for (let row_idx = 0; row_idx <= 8; row_idx++) {
             for (let col_idx = 0; col_idx <= 8; col_idx++){
