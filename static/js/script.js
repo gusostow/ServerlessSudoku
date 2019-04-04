7ef8aba661a51420af868a9e9503fe2a11e5a043
Augustus Ostow <ostowster@gmail.com>
2019-04-04 14:36:21 -0700

    Generate sudoku grid with js

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,47 @@
-function get_input_board() {
+function drawBoard() {
+    for (let rowIdx = 0; rowIdx <= 8; rowIdx++){
+        let rowElement = document.createElement("tr")
+        for (let colIdx = 0; colIdx <= 8; colIdx++){
+            let col = document.createElement("td")
+            let cell = document.createElement("input")
+            cell.setAttribute("class", "cell")
+            cell.setAttribute("id", "c" + rowIdx + colIdx)
+            cell.setAttribute("maxlength", "1")
+            cell.setAttribute("autocomplete", "off")
+            col.appendChild(cell)
+            rowElement.appendChild(col)
+        }
+        $("#board-tbody").append(rowElement)
+    }
+}
+
+$(document).ready(drawBoard)
+
+function getInputBoard() {
     let input_board = [];
-    for (let row_idx = 0; row_idx <= 8; row_idx++){
+    for (let rowIdx = 0; rowIdx <= 8; rowIdx++){
         let row = [];
-        for (let col_idx = 0; col_idx <= 8; col_idx++){
-            var cell_val = document.getElementById("c" + row_idx + col_idx).value;
-            if (cell_val == "") {
-                cell_val = "."
+        for (let colIdx = 0; colIdx <= 8; colIdx++){
+            var cellVal = document.getElementById("c" + rowIdx + colIdx).value;
+            if (cellVal == "") {
+                cellVal = "."
             }
-            row.push(cell_val)
+            row.push(cellVal)
         }
         input_board.push(row)
     }
     return input_board
 }
 
-function fill_board() {
-    let input_board = get_input_board()
+function fillBoard() {
+    let input_board = getInputBoard()
     const payload = JSON.stringify({board: input_board})
     const endpoint_url = "https://p3mto0h4h9.execute-api.us-east-2.amazonaws.com/development/sudoku"
     $.post(endpoint_url, payload, function(data) {
-        for (let row_idx = 0; row_idx <= 8; row_idx++) {
-            for (let col_idx = 0; col_idx <= 8; col_idx++){
-                let e = document.getElementById("c" + row_idx + col_idx)
-                e.value = data[row_idx][col_idx]
+        for (let rowIdx = 0; rowIdx <= 8; rowIdx++) {
+            for (let colIdx = 0; colIdx <= 8; colIdx++){
+                let e = document.getElementById("c" + rowIdx + colIdx)
+                e.value = data[rowIdx][colIdx]
             }
         }
     })
